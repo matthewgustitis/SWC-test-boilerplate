7020a7a6576233d5baa2b5ae61d35c789d06533b
matthewgustitis <matthew.gustitis@quadrabyte.net>
2022-04-29 15:47:41 -0500

    minor changes

--- copy_files/tests/test_test.py
+++ copy_files/tests/test_test.py
@@ -1,19 +1,10 @@
-""" test file """
+""" test file for general deployment and environment configuration"""
 
 from brownie import SimpleStorage, accounts
-
-"""
-def test_deploy():
-    account = accounts[0]
-    contract = SimpleStorage.deploy({"from": account})
-    assert contract, "Error: contract failed to deploy"
-"""
 
 
 def test_deploy(json_metadata):
     json_metadata["error_message"] = "Contract failed to deploy"
     account = accounts[0]
-    simple_storage = SimpleStorage.deploy({"from": account})
-    starting_value = simple_storage.retrieve()
-    expected = 1
-    assert starting_value == expected
+    contract = SimpleStorage.deploy({"from": account})
+    assert contract
