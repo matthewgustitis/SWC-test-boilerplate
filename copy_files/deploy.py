1496e467f09340db88f116af32167f4eae9ccd72
matthewgustitis <matthew.gustitis@quadrabyte.net>
2022-04-29 09:29:48 -0500

    initial commit

---
+++ copy_files/deploy.py
@@ -0,0 +1,10 @@
+from brownie import accounts, SimpleStorage
+
+
+def deploy_contract():
+    account = accounts[0]
+    SimpleStorage.deploy({"from": account})
+
+
+def main():
+    deploy_contract()
